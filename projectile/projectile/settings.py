056ef96524398ec034b2c72b3c87c525cdb47718
Kamrul Hasan <kamrul.h456@gmail.com>
2023-10-07 22:41:40 +0600

    separated silk on environment variable

--- projectile/projectile/settings.py
+++ projectile/projectile/settings.py
@@ -21,7 +21,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY", "")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-ENABLE_SILK = True
+ENABLE_SILK = os.environ.get("ENABLE_SILK", False)
 
 ALLOWED_HOSTS = []
 
