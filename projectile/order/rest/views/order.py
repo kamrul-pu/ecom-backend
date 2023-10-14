246a1c4657156b8f6d9f96de94007508da77e2dd
Kamrul Hasan <kamrul.h456@gmail.com>
2023-10-14 22:44:58 +0600

    filter applied on Order List

--- projectile/order/rest/views/order.py
+++ projectile/order/rest/views/order.py
@@ -1,4 +1,6 @@
 """Views for order."""
+from django_filters import rest_framework as filters
+
 from rest_framework.generics import ListAPIView, RetrieveUpdateAPIView
 
 from core.permissions import IsAdminUser, IsSuperAdmin
@@ -14,6 +16,15 @@ class AdminOrderList(ListAPIView):
     serializer_class = AdminOrderListSerializer
     permission_classes = (IsAdminUser | IsSuperAdmin,)
     queryset = Order().get_all_actives()
+    filter_backends = (filters.DjangoFilterBackend,)
+    filterset_fields = (
+        "customer",
+        "delivery_date",
+        "order_type",
+        "order_status",
+        "payment_method",
+        "payment_status",
+    )
 
 
 class AdminOrderDetail(RetrieveUpdateAPIView):
