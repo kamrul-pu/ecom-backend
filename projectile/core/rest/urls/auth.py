e360ae8a3dc0b1e697048c947a1263f0c1cca336
ayan-repliq <fazlul.ayan@repliq.dev>
2023-10-21 18:59:19 +0600

    views file name changed auth from register

--- projectile/core/rest/urls/auth.py
+++ projectile/core/rest/urls/auth.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from core.rest.views.register import UserRegistration
+from core.rest.views.auth import UserRegistration
 
 urlpatterns = [
     path("/register", UserRegistration.as_view(), name="user-register")
