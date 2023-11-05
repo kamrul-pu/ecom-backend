69ec49b9288a4dcc2e24f952a3968291bab2dbf9
Kamrul Hasan <kamrul.h456@gmail.com>
2023-11-05 23:06:14 +0600

    cart updated and fix admin panel issue

--- projectile/order/admin.py
+++ projectile/order/admin.py
@@ -10,51 +10,45 @@ from order.models import (
 
 
 class OrderAdmin(admin.ModelAdmin):
-    class Meta:
-        model = Order
-        list_display = (
-            "id",
-            "uid",
-            "order_type",
-            "order_status",
-            "payment_status",
-            "grand_total",
-            "status",
-        )
+    list_display = (
+        "id",
+        "uid",
+        "order_type",
+        "order_status",
+        "payment_status",
+        "grand_total",
+        "status",
+    )
 
 
 admin.site.register(Order, OrderAdmin)
 
 
 class OrderItemAdmin(admin.ModelAdmin):
-    class Meta:
-        model = OrderItem
-        list_display = (
-            "id",
-            "uid",
-            "order",
-            "product",
-            "quantity",
-            "price",
-            "total",
-            "status",
-        )
+    list_display = (
+        "id",
+        "uid",
+        "order",
+        "product",
+        "quantity",
+        "price",
+        "total",
+        "status",
+    )
 
 
 admin.site.register(OrderItem, OrderItemAdmin)
 
 
 class OrderPaymentAdmin(admin.ModelAdmin):
-    class Meta:
-        model = OrderPayment
-        list_display = (
-            "id",
-            "uid",
-            "customer",
-            "order",
-            "amount",
-            "status",
-        )
+    list_display = (
+        "id",
+        "uid",
+        "customer",
+        "order",
+        "amount",
+        "status",
+    )
 
 
 admin.site.register(OrderPayment, OrderPaymentAdmin)
--- projectile/product/admin.py
+++ projectile/product/admin.py
@@ -6,72 +6,64 @@ from product.models import Brand, Category, Manufacturer, Product
 
 
 class BrandAdmin(admin.ModelAdmin):
-    class Meta:
-        model = Brand
-        list_display = (
-            "id",
-            "uid",
-            "slug",
-            "name",
-            "origin",
-            "popularity",
-            "status",
-            "created_at",
-        )
+    list_display = (
+        "id",
+        "uid",
+        "slug",
+        "name",
+        "origin",
+        "popularity",
+        "status",
+        "created_at",
+    )
 
 
 admin.site.register(Brand, BrandAdmin)
 
 
 class CategoryAdmin(admin.ModelAdmin):
-    class Meta:
-        model = Category
-        list_display = (
-            "id",
-            "uid",
-            "slug",
-            "name",
-            "status",
-            "created_at",
-        )
+    list_display = (
+        "id",
+        "uid",
+        "slug",
+        "name",
+        "status",
+        "created_at",
+    )
 
 
 admin.site.register(Category, CategoryAdmin)
 
 
 class ManufacturerAdmin(admin.ModelAdmin):
-    class Meta:
-        model = Manufacturer
-        list_display = (
-            "id",
-            "uid",
-            "slug",
-            "name",
-            "origin",
-            "popularity",
-            "status",
-            "created_at",
-        )
+    list_display = (
+        "id",
+        "uid",
+        "slug",
+        "name",
+        "origin",
+        "popularity",
+        "status",
+        "created_at",
+    )
 
 
 admin.site.register(Manufacturer, ManufacturerAdmin)
 
 
 class ProductAdmin(admin.ModelAdmin):
-    class Meta:
-        model = Product
-        list_display = (
-            "id",
-            "uid",
-            "slug",
-            "name",
-            "status",
-            "stock",
-            "is_published",
-            "mrp",
-            "discounted_price",
-            "created_at",
-        )
+    list_display = (
+        "id",
+        "uid",
+        "slug",
+        "name",
+        "status",
+        "stock",
+        "is_published",
+        "mrp",
+        "discounted_price",
+        "created_at",
+    )
 
 
 admin.site.register(Product, ProductAdmin)
