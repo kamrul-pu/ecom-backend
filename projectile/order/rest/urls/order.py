cde50d79401a94213793632e0fa18783f570df7a
Kamrul Hasan <kamrul.h456@gmail.com>
2023-11-09 23:36:08 +0600

    customer order list create endpoint created:

--- projectile/order/rest/urls/order.py
+++ projectile/order/rest/urls/order.py
@@ -5,9 +5,11 @@ from django.urls import path
 from order.rest.views.order import (
     AdminOrderList,
     AdminOrderDetail,
+    CustomerOrderList,
 )
 
 urlpatterns = [
-    path("", AdminOrderList.as_view(), name="admin-order"),
-    path("/<uuid:uid>", AdminOrderDetail.as_view(), name="admin-order-details"),
+    path("", CustomerOrderList.as_view(), name="customer-order-list-create"),
+    path("/admin", AdminOrderList.as_view(), name="admin-order"),
+    path("/admin/<uuid:uid>", AdminOrderDetail.as_view(), name="admin-order-details"),
 ]
--- projectile/order/rest/views/order.py
+++ projectile/order/rest/views/order.py
@@ -1,21 +1,33 @@
 """Views for order."""
 from django_filters import rest_framework as filters
 
-from rest_framework.generics import ListAPIView, RetrieveUpdateAPIView
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.generics import (
+    CreateAPIView,
+    ListAPIView,
+    ListCreateAPIView,
+    RetrieveUpdateAPIView,
+)
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
 from core.permissions import IsAdminUser, IsSuperAdmin
+from common.choices import Status
+
 from order.models import Order
-from order.choices import OrderType
+from order.choices import OrderType, OrderStatus
 from order.rest.serializers.order import (
     AdminOrderListSerializer,
     AdminOrderDetailSerializer,
+    OrderListSerializer,
 )
 
 
 class AdminOrderList(ListAPIView):
     serializer_class = AdminOrderListSerializer
     permission_classes = (IsAdminUser | IsSuperAdmin,)
-    queryset = Order().get_all_actives()
+    queryset = Order().get_all_actives().prefetch_related("order_items")
     filter_backends = (filters.DjangoFilterBackend,)
     filterset_fields = (
         "customer",
@@ -32,3 +44,43 @@ class AdminOrderDetail(RetrieveUpdateAPIView):
     serializer_class = AdminOrderDetailSerializer
     queryset = Order().get_all_actives()
     lookup_field = "uid"
+
+
+class CustomerOrderList(ListCreateAPIView):
+    permission_classes = (IsAuthenticated,)
+    serializer_class = OrderListSerializer
+
+    def get_queryset(self):
+        user = self.request.user
+        queryset = (
+            Order()
+            .get_all_actives()
+            .filter(
+                customer_id=user.id,
+                order_type=OrderType.ORDER,
+            )
+            .prefetch_related("order_items")
+        )
+
+        return queryset
+
+    def post(self, request, *args, **kwargs):
+        user = request.user
+        cart, created = Order.objects.get_or_create(
+            customer_id=user.id,
+            order_type=OrderType.CART,
+            status=Status.ACTIVE,
+        )
+        if created or cart.order_items.count() == 0:
+            return Response(
+                {"detail": "You do not have any item in cart."},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+        cart.order_type = OrderType.ORDER
+        cart.order_status = OrderStatus.PENDING
+        cart.save(update_fields=["order_type", "order_status"])
+
+        return Response(
+            {"order": self.serializer_class(cart).data}, status=status.HTTP_201_CREATED
+        )
